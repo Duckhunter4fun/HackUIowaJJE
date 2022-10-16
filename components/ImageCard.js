99c6835b09d4e71c3adab32d56cb7e078d2804d3
joe <joseph.white.bsa@gmail.com>
2022-10-16 11:19:26 -0500

    new component and images for details

---
+++ components/ImageCard.js
@@ -0,0 +1,51 @@
+import { Card, Image, Text, Group, Badge, Button, ActionIcon, createStyles } from '@mantine/core';
+import Link from 'next/link';
+
+const useStyles = createStyles((theme) => ({
+  card: {
+    backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[7] : theme.white,
+  },
+
+  section: {
+    borderBottom: `1px solid ${
+      theme.colorScheme === 'dark' ? theme.colors.dark[4] : theme.colors.gray[3]
+    }`,
+    paddingLeft: theme.spacing.md,
+    paddingRight: theme.spacing.md,
+    paddingBottom: theme.spacing.md,
+  },
+
+  like: {
+    color: theme.colors.red[6],
+  },
+
+  label: {
+    textTransform: 'uppercase',
+    fontSize: theme.fontSizes.xs,
+    fontWeight: 700,
+  },
+}));
+
+
+export function ImageCard({ image, title, caption}) {
+  const { classes, theme } = useStyles();
+
+  return (
+    <Card withBorder radius="md" p="md" className={classes.card}>
+      <Card.Section>
+        <Image src={image} alt={title} height={180} />
+      </Card.Section>
+
+      <Card.Section className={classes.section} mt="md">
+        <Group position="apart">
+          <Text size="lg" weight={500}>
+            {title}
+          </Text>
+        </Group>
+        <Text size="sm" mt="xs">
+          {caption}
+        </Text>
+      </Card.Section>
+    </Card>
+  );
+}
